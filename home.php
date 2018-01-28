8e63acb218bdd1c1dc89b282e69c741f8abd5687
Zachary Williams <zachwilliams.services@gmail.com>
2018-01-28 01:39:22 -0500

    Added home page and associated stylesheet files.

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("sitewide/opener.php");
+?>
+    <title>Make N Share</title>
+<?php
+    require_once("sitewide/header.php"); 
+?>
+
+
+<div class="container">
+    <div id="header">
+        <img src="img/gray-logo.png" id="logo">
+        <a href="profile.php" id="profile-button">PROFILE</a>
+        <a href="upload.php" id="upload-button">UPLOAD</a>
+    </div>
+    <div id="content">
+        
+    </div>
+    <div id="footer">
+        
+    </div>
+</div>
+
+
+
+<?php 
+    require_once("sitewide/footer.php");
+?>
